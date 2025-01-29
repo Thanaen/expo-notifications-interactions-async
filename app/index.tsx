a1f7cc7be017067f2bce5fd84ffdac70bb81a8fa
Clément Costedoat <ccostedoat@isdecisions.com>
2025-01-29 16:53:20 +0100

    feat: add logs to local storage to test on physical device

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
 import { Button, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
 import * as React from "react";
+import { MMKV, useMMKVString } from "react-native-mmkv";
 
+const storage = new MMKV();
 const NOTIFICATION_CATEGORY = "test-notification-category";
 
 Notifications.setNotificationHandler({
@@ -11,6 +13,31 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
+
+const logToStorage = (message: string) => {
+  const prev = storage.getString("logs") || "";
+
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const milliseconds = now.getMilliseconds().toString().padStart(3, "0");
+  const time = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+  const formattedMessage = `[${time}] ${message}`;
+
+  if (__DEV__) {
+    console.log(formattedMessage);
+  }
+
+  storage.set("logs", `${prev}\n${formattedMessage}`);
+};
+
+const Logs = () => {
+  const [logs] = useMMKVString("logs");
+
+  return <Text>{logs ?? "No logs"}</Text>;
+};
 
 export default function Index() {
   React.useEffect(() => {
@@ -39,19 +66,21 @@ export default function Index() {
       Notifications.addNotificationResponseReceivedListener(
         async (response) => {
           if (response.actionIdentifier === "async-action") {
-            console.log("Doing async action");
-            console.log(
+            logToStorage("Doing async action");
+            logToStorage(
               "Sync actions will be shown in the console when the app is in background"
             );
-            console.log("But let's try to fetch some data from the server...");
+            logToStorage("But let's try to fetch some data from the server...");
             const data = await fetch(
               "https://jsonplaceholder.typicode.com/todos/1"
             ).then((response) => response.json());
-            console.log("Data fetched successfully", data);
-            console.log("Fetching only started when the app was in foreground");
+            logToStorage("Data fetched successfully");
+            logToStorage(
+              "Fetching only started when the app was in foreground"
+            );
           } else if (response.actionIdentifier === "sync-action") {
-            console.log("Doing sync action");
-            console.log(
+            logToStorage("Doing sync action");
+            logToStorage(
               "All logs will be shown in the console, even if the app is in background"
             );
           }
@@ -94,7 +123,14 @@ export default function Index() {
         title="Show Notification in 3 seconds"
         onPress={showNotification}
       />
+      <Button
+        title="Clear logs"
+        onPress={() => {
+          storage.delete("logs");
+        }}
+      />
       <Text>(Put the app in background after pressing)</Text>
+      <Logs />
     </View>
   );
 }
